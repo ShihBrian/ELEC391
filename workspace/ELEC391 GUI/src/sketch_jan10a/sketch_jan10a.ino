dc4afcb929f447750c5c9be7c8fef0633aa63155
Brian <brianshih33@hotmail.com>
2017-01-19 09:51:17 -0800

    Add optical encoder counter and extract velocity and acceleration from the data.

--- workspace/ELEC391 GUI/src/sketch_jan10a/sketch_jan10a.ino
+++ workspace/ELEC391 GUI/src/sketch_jan10a/sketch_jan10a.ino
@@ -1,8 +1,30 @@
 #include <SoftwareSerial.h>
+#define LEDPin 13
 
 SoftwareSerial mySerial(10,11);
+/*===============================*/
+//Globals for optical encoder
+#define ENCODERAPIN 2 //interrupt
+#define ENCODERBPIN 36 //can be interrupt for better performance Pin 2, 3, 18, 19, 20, 21
+#define CPR 100 //100 ticks per 360 degrees
+#define CLOCKWISE 1
+#define COUNTERCLOCKWISE 2
+volatile long encoderPos = 0.0;
+volatile long interruptsReceived = 0;
+short currentdirection = CLOCKWISE;
+long previousPos = 0;
+volatile double angle = 0.0;
+volatile double old_angle = 0.0;
+double curr_vel;
+double old_vel = 0.0;
+unsigned long curr_time;
+unsigned long start_time = millis();
+double deltaT = 10.0; //ms
+double accel;
+double scale = 1/(deltaT/1000);
+/*===============================*/
 
-unsigned long tick_start = millis();;
+unsigned long tick_start = millis();
 unsigned long tickms = 10;
 unsigned long curr_tick;
 int x = 10;
@@ -11,15 +33,20 @@ unsigned int startbyte = 0xFF;
 unsigned int endbyte = 0xFE;
 int dx = 1;
 int dy = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
+  pinMode(ENCODERAPIN, INPUT);
+  pinMode(ENCODERBPIN, INPUT);
+  pinMode(LEDPin, OUTPUT);
+  attachInterrupt(0,ISR_GPIO, RISING);
   while(!Serial){
   }
-  delay(1500);
+  delay(1000);
 }
 
-void Send_Data(int padding, unsigned int data)
+void Add_Padding(int padding, unsigned int data)
 {
   if (padding == 2)
   {
@@ -37,39 +64,21 @@ void Send_Data(int padding, unsigned int data)
   }
 }
 
-void loop() {
-  int stack[] = {0,0,0};
-  int idx=2;
-  int temp;
-
-  curr_tick = millis();
-  if(curr_tick-tickms >= tick_start){
-    
-    Send_Data(1,startbyte);
-    
-    if(x < 16){
-      Send_Data(2,x);   
+void Send_Data(int value)
+{
+    if(value < 16){
+      Add_Padding(2,value );   
     }
-    else if (x < 255){
-      Send_Data(1,x); 
+    else if (value  < 256){
+      Add_Padding(1,value ); 
     }
     else{
-      Send_Data(0,x); 
-    }
-    if(y < 16){
-      Send_Data(2,y);
-    }
-    else if (y < 255){
-      Send_Data(1,y); 
-    }
-    else{
-      Send_Data(0,y); 
-    }
-    
-    Send_Data(1,endbyte); 
-    
-    tick_start = millis();
+      Add_Padding(0,value ); 
+    }  
+}
 
+void test_pattern()
+{
     x += dx;
     y += dy;
     if (x == 240 && y == 10){
@@ -87,7 +96,76 @@ void loop() {
     if(y == 10 && x == 10){
       dx = 1;
       dy = 0;
-    }    
+    }      
+}
+
+void loop() {
+  curr_tick = millis();
+  /*if(curr_tick-tickms >= tick_start){
+    Send_Data(startbyte);
+    Send_Data(x);
+    Send_Data(y);
+    Send_Data(endbyte);  
+    tick_start = millis();
+    test_pattern();
+  }*/
+  curr_time = millis();
+  if(previousPos != encoderPos){
+    if(curr_time-deltaT >= start_time){
+      if(old_angle > angle && currentdirection == CLOCKWISE){
+        curr_vel = ((angle + (360 - (old_angle)))/deltaT)*scale; 
+      }
+      else if(old_angle < angle && currentdirection == COUNTERCLOCKWISE){
+        curr_vel = ((angle - (360 + (old_angle)))/deltaT)*scale; 
+      }
+      else{
+        curr_vel = ((angle - old_angle)/deltaT)*scale;
+      }
+
+      accel = ((abs(curr_vel) - abs(old_vel))/deltaT)*scale;
+      
+      Serial.print(angle);
+      Serial.print("\t");
+      Serial.print(old_angle);
+      Serial.print("\t");
+      Serial.print(curr_vel); //degrees/second
+      Serial.print("\t");
+      Serial.print(old_vel);
+      Serial.print("\t");
+      Serial.println(accel); //degrees/second^2
+      previousPos = encoderPos;
+      old_angle = angle;
+      old_vel = curr_vel;
+      start_time = millis();
+    }
   }
 }
 
+void ISR_GPIO()
+{
+  // read both inputs
+  int a = digitalRead(ENCODERAPIN);
+  int b = digitalRead(ENCODERBPIN);
+ 
+  if (a == b )
+  {
+    
+    // b is leading a (counter-clockwise)
+    encoderPos--;
+    currentdirection = COUNTERCLOCKWISE;
+  }
+  else
+  {
+    // a is leading b (clockwise)
+    encoderPos++;
+    currentdirection = CLOCKWISE;
+  }
+ 
+  // track 0 to 100
+  encoderPos = encoderPos % CPR;
+
+  angle = 3.6 * encoderPos;
+ 
+  // track the number of interrupts
+  interruptsReceived++;
+}
