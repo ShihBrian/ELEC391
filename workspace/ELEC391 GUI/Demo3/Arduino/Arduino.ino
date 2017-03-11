5b0d7f3e2fb94217e7a7f810a5a22951919bfeab
Brian <brianshih33@hotmail.com>
2017-03-11 00:45:39 -0800

    Create new folder for Demo3. Rename variables in both Arduino and python.

---
+++ workspace/ELEC391 GUI/Demo3/Arduino/Arduino.ino
@@ -0,0 +1,361 @@
+#include <PID_v1.h>
+#include <SoftwareSerial.h>
+
+#define FWPinL 4
+#define BWPinL 5
+#define FWPinR 6
+#define BWPinR 7
+
+//#define DEBUG
+//#define LOCAL_OUTPUT
+SoftwareSerial mySerial(10,11);
+/*===============================*/
+//Globals for optical encoder
+#define FORWARD 1
+#define BACKWARD 2
+volatile long encoderPosLeft = 10;
+volatile long encoderPosRight = 10;
+short currDirectionLeft = FORWARD;
+short currDirectionRight = FORWARD;
+long previousPosLeft = 0;
+long previousPosRight = 0;
+volatile double extensionLeft = 0;
+volatile double extensionRight= 0;
+long PosLeftoffset = -10;
+long PosRightoffset = -10;
+volatile long countData = 0;
+/*===============================*/
+
+/*===============================*/
+//Communication globals
+unsigned int startbyte = 0xFF;
+unsigned int endbyte = 0xFE;
+unsigned int incomingByte = 0;
+int state = 0;
+
+enum MsgType{
+  encoderleft = 1,
+  encoderright = 2,
+  desiredleft = 3,
+  desiredright = 4,
+  debug = 5,
+  start = 6,
+};
+/*===============================*/
+
+/*===============================*/
+//PID globals
+float Kp = 50;
+float Ki = 0;
+float Kd = 5;
+double DesiredLeft,PIDOutputLeft;
+double DesiredRight,PIDOutputRight;
+double ActualLeft = 0;
+double ActualRight = 0;
+PID PID1(&ActualLeft, &PIDOutputLeft, &DesiredLeft, Kp, Ki, Kd, DIRECT);
+PID PID2(&ActualRight, &PIDOutputRight, &DesiredRight, Kp, Ki, Kd, DIRECT);
+const int sampleRate = 10; // Variable that determines how fast our PID loop runs
+const long serialPing = 100;
+unsigned long now = 0;
+unsigned long lastMessage = 0;
+int rxPID = false;
+int PIDrx_count = 0;
+int P, I, D;
+/*===============================*/
+
+unsigned long tick_start = millis();
+unsigned long tickms = 10;
+unsigned long curr_tick;
+unsigned long curr_time;
+unsigned long start_time = millis();
+bool pause = false;
+bool intersect = false;
+
+int desired_angle1 = 0;
+int desired_angle2 = 0;
+int direction1;
+int direction2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(10);
+  pinMode(FWPinL, OUTPUT);
+  pinMode(BWPinL, OUTPUT);
+  pinMode(FWPinR, OUTPUT);
+  pinMode(BWPinR, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW); // /RST resets the internal counter between runs
+  delay(10);
+  digitalWrite(9, HIGH); // Stay high for rest of the run
+  // Set all pins in PORTA and C (digital pins 22->37) as input pins
+  for(int i = 22; i<38; i++) {
+    pinMode(i, INPUT);
+  }
+  // Set pins 5,6,7 in PORT B (digital pins 11->16) as output pins
+  for(int j = 11; j<17; j++) {
+    pinMode(j, OUTPUT);
+    digitalWrite(j, LOW);
+    
+  }
+  PID1.SetMode(AUTOMATIC);
+  PID2.SetMode(AUTOMATIC);
+  PID1.SetSampleTime(sampleRate);
+  PID2.SetSampleTime(sampleRate);
+  while(!Serial){ //wait for serial to be initialized
+  }
+  Send_Message(start,1);
+  delay(500);
+}
+
+void loop() {
+#if !defined(DEBUG) && !defined(LOCAL_OUTPUT) //main loop 
+  static int tries = 0;
+  curr_tick = millis();
+  if(!pause){
+    if(curr_tick-tickms >= tick_start){ //run loop every tickms 
+      ISR_Counter(); //get new encoder positions
+      if((previousPosLeft != encoderPosLeft)||(previousPosRight != encoderPosRight)){
+        if(tries == 2){ //send message every second encoderPosLeft change
+          Send_Message(encoderleft, encoderPosLeft); 
+          previousPosLeft = encoderPosLeft;
+          //Send_Message(encoderright, encoderPosRight); 
+          //previousPosRight = encoderPosRight; 
+          tries = 0;
+        }
+        else{
+          tries++;
+        }
+      }
+      tick_start = millis();
+    }
+  }
+
+  if(Serial.available()>0) {
+    incomingByte = Serial.read();
+    
+    if(rxPID == true){
+      if(PIDrx_count == 0){
+        P = incomingByte;
+      }
+      else if(PIDrx_count == 1)
+        I = incomingByte;
+      else if(PIDrx_count == 2)
+        D = incomingByte;
+      PIDrx_count++;
+      if(PIDrx_count == 3){
+        PIDrx_count = 0;
+        rxPID = false;
+        PID1.SetTunings(P,I,D);
+        PID2.SetTunings(P,I,D);
+        Send_Message(debug, P);
+        Send_Message(debug, I);
+        Send_Message(debug, D);
+      } 
+    }
+    
+    if(incomingByte == 0xFF){     
+      intersect = true;
+    }
+    else if(incomingByte == 0xFD){
+      intersect = false;
+      state = 0;
+      all_off();   
+    }
+    else if(incomingByte == 0xFE){
+      pause = !pause;
+    }
+    else if(incomingByte == 0xFC){
+      rxPID = true;
+    }
+    
+    if (state == 0){
+      if(incomingByte == 0xFB){
+        state = 1;
+      }
+    }
+    else if(state == 1){
+      desired_angle1 = incomingByte;
+      state = 2;
+    }
+    else if(state == 2){
+      desired_angle2 = incomingByte;
+      state = 3;
+    }
+    else if(state == 3){
+      if(incomingByte == 0xFA){
+        state = 0;
+      }
+    } 
+    
+    }
+    //pass values to PID library to compute output power
+    PID_Output(intersect, desired_angle1, desired_angle2);
+#endif  
+#ifdef DEBUG //for debugging and testing PID
+  /*static int iter = 0;
+  ISR_Counter();
+  PID_Output(true, desired_angle1, desired_angle2);
+  
+  now = millis(); //Keep track of time
+  if(now - lastMessage > serialPing) { //Move arm to new position every serialPing ms
+    if (iter== 2){
+      desired_angle1 = 160;
+      desired_angle2 = 68;
+      all_off();
+    }
+    else if(iter ==4){
+      desired_angle1 = 68;
+      desired_angle2 = 160;     
+      all_off();
+    }
+    else if(iter==6){
+      desired_angle1 = 90;
+      desired_angle2 = 90;     
+      all_off();
+    }
+    else if(iter ==8){
+      desired_angle1 = 121;
+      desired_angle2 = 121;     
+      all_off();
+      iter = 0;
+    }
+    iter++;   
+    lastMessage = now; 
+  }  */
+  
+#endif //DEBUG
+
+#ifdef LOCAL_OUTPUT //for outputting encoder information on local arduino serial
+ISR_Counter();
+  if((encoderPosLeft != previousPosLeft) || (encoderPosRight != previousPosRight)){
+    Serial.print(encoderPosLeft);
+    Serial.print("\t");
+    Serial.println(encoderPosRight);
+    previousPosLeft = encoderPosLeft;
+    previousPosRight = encoderPosRight;
+  }
+#endif //LOCAL_OUTPUT
+}
+
+int home_position(){
+}
+
+void PID_Output(bool flag, int desired_angle1, int desired_angle2){
+    if (flag){
+      //Run PID computation
+      PID1.Compute();
+      PID2.Compute();
+      //Turn on pin according to direction and PID output. Limit to 255 
+      if (direction1 == FORWARD){
+        analogWrite(FWPinL, PIDOutputLeft>255? 255 : PIDOutputLeft);
+        digitalWrite(BWPinL, LOW);
+      }
+      else{
+        analogWrite(BWPinL, PIDOutputLeft>255? 255 : PIDOutputLeft);
+        digitalWrite(FWPinL, LOW);
+      }
+      if (direction2 == FORWARD){
+        analogWrite(FWPinR, PIDOutputRight>255? 255 : PIDOutputRight);
+        digitalWrite(BWPinR, LOW);
+      }
+      else{
+        analogWrite(BWPinR, PIDOutputRight>255? 255 : PIDOutputRight);
+        digitalWrite(FWPinR, LOW);
+      }
+    }
+}
+
+//function to read 32bit data from encoder chip
+void ISR_Counter(){
+  digitalWrite(13, HIGH); // Set OE to HIGH (disable)
+  
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH); // SEL1 = 0 and SEL2 = 1
+  
+  digitalWrite(13, LOW); // Set OE to LOW (enable)
+  byte MSBresult = getbyte();
+  byte MSBresult2 = getbyte2();
+  
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH); // SEL1 = 1 and SEL2 = 1
+  byte secondResult = getbyte();
+  byte secondResult2 = getbyte2();
+  
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW); // SEL1 = 0 and SEL2 = 0
+  byte thirdResult = getbyte();
+  byte thirdResult2 = getbyte2();
+  
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW); // SEL1 = 1 and SEL2 = 0
+  byte LSBresult = getbyte();
+  byte LSBresult2 = getbyte2();
+
+  digitalWrite(13, HIGH); // Set OE to HIGH (disable)
+  
+  encoderPosLeft = -1*PosLeftoffset+mergeFunc(MSBresult, secondResult, thirdResult, LSBresult);
+  encoderPosRight = -1*PosRightoffset+mergeFunc(MSBresult2, secondResult2, thirdResult2, LSBresult2);
+
+  if (encoderPosLeft < 0) encoderPosLeft = 0;
+  if (encoderPosRight < 0) encoderPosRight = 0;
+}
+
+//get length of data being sent out
+int get_length(long data){
+  if(int(data/16) == 0){
+    return 1;
+  }
+  else if(int(data/16) < 16){
+    return 2;
+  }
+  else{
+    return 3;
+  } 
+}
+//Send message in the following order: startbyte, type, length, data, endbyte
+void Send_Message(MsgType type,long data){
+  int data_length = get_length(data);
+  Serial.print(startbyte, HEX);
+  Serial.print(type, HEX);
+  Serial.print(data_length, HEX);
+  Serial.print(data, HEX);
+  Serial.print(endbyte, HEX);
+}
+//turn all motor control pins off
+void all_off(){
+  digitalWrite(FWPinL, LOW);
+  digitalWrite(BWPinL, LOW);
+  digitalWrite(FWPinR, LOW);
+  digitalWrite(BWPinR, LOW);      
+}
+
+//function to get individual bytes from encoder chip
+byte getbyte(){
+/*Get stable data for the most significant byte of countData*/
+  byte MSBold = PINA;       // read datapins D0->D7 and store in MSBold
+  byte MSBnew = PINA;       // read again immediatly after to assure stable data
+  if (MSBnew == MSBold){ 
+    byte MSBresult = MSBnew;
+    return MSBresult;  
+  }
+  else getbyte();
+}
+byte getbyte2(){
+/*Get stable data for the most significant byte of countData*/
+  byte MSBold = PINC;       // read datapins D0->D7 and store in MSBold
+  byte MSBnew = PINC;       // read again immediatly after to assure stable data
+  if (MSBnew == MSBold){ 
+    byte MSBresult = MSBnew;
+    return MSBresult;  
+  }
+  else getbyte();
+}
+//merge 4 bytes into a signed 32bit value
+long mergeFunc(byte MSBresult, byte secondResult, byte thirdResult, byte LSBresult){
+/*Merges the 4 bytes returning one 32-bit variable called countData*/
+  long tempVar = 0;
+  tempVar |= ((long) MSBresult << 24) | ((long) secondResult << 16) | ((long) thirdResult << 8) | ((long) LSBresult << 0);
+  countData = tempVar;
+  return countData;
+}
---
+++ workspace/ELEC391 GUI/Demo3/Main.py
@@ -0,0 +1,506 @@
+import sys
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+import numpy as np
+import serial
+import math
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from enum import Enum
+from matplotlib import style
+import time
+from scipy.optimize import fsolve
+
+
+#==============Plot Globals===============#
+wHeight = 508
+wWidth = 501
+x_range = 160
+y_start = 0
+y_end = 140
+y_range = y_end - y_start
+style.use("ggplot")
+f = Figure(figsize=(5.4,5.3), dpi=120)
+a = f.add_subplot(111)
+b = f.add_subplot(111)
+a.set_xlim([-1*x_range/2,x_range/2])
+a.set_ylim([y_start,y_end])
+x_c = 0
+y_c = 0
+left_length = 10
+right_length = 10
+base_length = 10   
+x_conv =(wWidth/x_range)
+y_conv = (wWidth/y_range)
+mouse_x = 0
+mouse_y = 0
+#=========================================#
+
+#=========Communication Globals===========#
+class States(Enum):
+    RxStart = 1
+    RxMsgType = 2
+    RxMsgLength = 3
+    RxMsg = 4
+    RxEnd = 5
+    
+class eMsgType(Enum):
+    encoderPosLeft = 1
+    encoderPosRight = 2
+    desiredLeft = 3
+    desiredRight = 4
+    debug = 5
+    start =6;
+    
+PauseByte = b"\xFE"
+IntersectTrue = b"\xFF"
+IntersectFalse = b"\xFD"
+MsgType = 0
+state = States.RxStart
+nxtstate = States.RxStart
+MsgLength = 0
+Startbyte = 255
+EndByte = 254
+success_count = 0
+startrx= 0;
+#==========================================#
+
+#===========Constraint Globals=============#
+class Shape(Enum):
+    Rectangle = 1
+    Circle = 2
+    
+sqX=-15
+sqY=85
+sqL=30
+sqH=40
+default_sqX=-15
+default_sqY=85
+default_sqL=30
+default_sqH=40
+circR = 25
+circX = 125
+circY = 125
+curr_shape = Shape.Rectangle
+manual_pos = 0;
+#==========================================#
+
+#==============PID Globals=================#
+P = 10;
+I = 0;
+D = 1;
+Wall_P = 100
+Wall_D = 10
+Spring_P = 10
+Spring_D = 0
+Damper_P = 10
+Damper_D = 1
+#==========================================#
+pause = 0
+intersect_time = time.time()
+intersect_delay = 1
+start_time = time.time()
+start_time2 = time.time()*1000
+time2_delay = 250
+
+class StatusBar(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.label = Label(self, bd=1, anchor=W, relief="solid", borderwidth =2, font=("Courier", 20), width = 6)
+        self.label.pack()
+
+    def set(self, format, *args):
+        self.label.config(text=format % args)
+        self.label.update_idletasks()
+
+    def clear(self):
+        self.label.config(text="")
+        self.label.update_idletasks()
+
+def draw_constraint(shape, sqX, sqY, sqL, sqH): 
+    global circX, circY, circR, curr_shape, rectangle, manual_pos
+    manual_pos = 0
+    w.delete(rectangle)
+    curr_shape = shape
+    sqX = abs(sqX+x_range/2)
+    if shape == Shape.Rectangle:
+        rectangle = w.create_rectangle(sqX*x_conv,wHeight-sqY*y_conv+(y_conv*y_start),(sqX+sqL)*x_conv,\
+                            wHeight-(sqY+sqH)*y_conv+(y_conv*y_start), fill="lightblue")
+        create_entry(Shape.Rectangle)
+    if shape == Shape.Circle:
+        circle = w.create_oval((circX-circR)*x_conv,wHeight-(circY-circR)*y_conv,\
+                      (circX+circR)*(wWidth/150),wHeight-(circY+circR)*(wWidth/150), fill="lightblue")
+        create_entry(Shape.Circle)
+
+bOnce = 1;
+bOnce2 = 1;
+def rectIntersect(pX, pY):
+    global sqX, sqY, sqH, sqL, intersect_time, intersect_delay,start_time2
+    cTRx = sqX+sqL
+    cBLy = sqY+sqH  
+    if (pX >= sqX and pX <= cTRx) and (pY >= sqY and pY <= cBLy):
+        Send_Intersect_Flag(TRUE)
+        xLeft = pX - sqX
+        xRight = cTRx - pX
+        yUp = pY - sqY
+        yDown = cBLy - pY
+        minimum = min(xLeft,xRight,yUp,yDown)
+        if xLeft == minimum:
+            desired_x = sqX-4
+            desired_y = pY
+        elif xRight == minimum:
+            desired_x = cTRx+4
+            desired_y = pY
+        elif yUp == minimum:
+            desired_x = pX
+            desired_y = sqY-4
+        else:
+            desired_x = pX
+            desired_y = cBLy+4
+        if start_time2+time2_delay < time.time()*1000:
+            left_length, right_length = inverse_kinematic(desired_x, desired_y)
+            stDesired1.set("%d", left_length)
+            stDesired2.set("%d", right_length)            
+            ser.write(b"\xFB")
+            value = [int(left_length)]
+            length = bytes(value)
+            ser.write(length)
+            value = [int(right_length)]
+            length = bytes(value)
+            ser.write(length)
+            ser.write(b"\xFA")
+            start_time2 = time.time()*1000
+    else:
+        Send_Intersect_Flag(FALSE)
+        stDesired1.set("%s", "")
+        stDesired2.set("%s", "")
+
+def circIntersect(pX, pY):
+    global circX, circY, circR
+    distance = np.sqrt(np.square(circX-pX) + np.square(circY-pY))
+    if distance < circR:
+        Send_Intersect_Flag(TRUE)
+        if circR-distance > 9:
+            ser.write("9".encode())
+        else:
+            ser.write(str(int(circR-distance)).encode())
+    else:
+        Send_Intersect_Flag(FALSE)
+        
+def Send_Intersect_Flag(flag):
+    global bOnce, bOnce2
+    if flag:
+        if bOnce:
+            bOnce2 = 1
+            ser.write(IntersectTrue)
+            bOnce = 0
+    else:
+        if bOnce2:
+            if start_time+1 < time.time():
+                bOnce = 1
+                ser.write(IntersectFalse)
+                bOnce2 = 0 
+                             
+def create_entry(type):
+    global entryLength, entryHeight, entryX, entryY, XYLabel, LengthLabel, WidthLabel   
+    if type == Shape.Rectangle:
+        LengthLabel.config(text="Length")
+        XYLabel.config(text="Bottom Left XY")
+        WidthLabel.config(text="Height")
+        entryHeight.configure(state='normal')
+    if type == Shape.Circle:
+        LengthLabel.config(text="Radius")
+        XYLabel.config(text="Center XY")
+        WidthLabel.config(text="")
+        entryHeight.configure(state='disabled')
+        
+ 
+def entry_callback(event):
+    global sqX, sqY, sqH, sqL, circR, circX, circY, curr_shape, P, I, D
+    if len(entryX.get()) != 0:
+        if curr_shape == Shape.Rectangle:
+            sqX = int(entryX.get())
+            sqY = int(entryY.get())
+            sqL = int(entryLength.get())
+            sqH = int(entryHeight.get())
+        elif curr_shape == Shape.Circle:
+            circR = int(entryLength.get())
+            circX = int(entryX.get())
+            circY = int(entryY.get())
+        draw_constraint(curr_shape, sqX, sqY, sqL, sqH) 
+    if len(entryP.get()) != 0:
+        Set_PID(entryP.get(),entryI.get(),entryD.get())
+
+def Set_PID(P,I,D):
+    txP = [int(P)]
+    txI = [int(I)]
+    txD = [int(D)]
+    ser.write(b"\xFC")
+    ser.write(txP)
+    ser.write(txI)
+    ser.write(txD)
+    
+def create_buttons(root):
+    ttk.Button(root, text="Square", command=lambda: draw_constraint(Shape.Rectangle, default_sqX, default_sqY, default_sqL, default_sqH)).place(x=0,y=0)
+    ttk.Button(root, text="Circle", command=lambda: draw_constraint(Shape.Circle)).place(x=75,y=0)
+    ttk.Button(root, text="Pause", command=lambda: pause_update()).place(x=500,y=0)
+    ttk.Button(root, text="Clear", command=lambda: clear()).place(x=575,y=0)
+    ttk.Button(root, text="Wall", command=lambda: Set_PID(Wall_P, 0, Wall_D)).place(x=660,y=500)
+    ttk.Button(root, text="Spring", command=lambda: Set_PID(Spring_P, 0, Spring_D)).place(x=740,y=500)
+    ttk.Button(root, text="Button", command=lambda: Set_PID(Damper_P, 0, Damper_D)).place(x=820,y=500)
+
+def create_labels(root):
+    Label(root, text="Rx Count", font=("Courier", 18)).place(x=660,y=10)
+    Label(root, text="X Pos", font=("Courier", 20)).place(x=660,y=40)
+    Label(root, text="Y Pos", font=("Courier", 20)).place(x=660,y=70)
+    Label(root, text="Ext Left", font=("Courier", 14)).place(x=670, y=155)
+    Label(root, text="Ext Right", font=("Courier", 14)).place(x=770, y=155)
+    Label(root, text="Desired1", font=("Courier", 14)).place(x=670, y=215)
+    Label(root, text="Desired2", font=("Courier", 14)).place(x=780, y=215)
+    
+def init_window():
+    root = Tk()
+    root.geometry('900x700+200+80')
+    tk.Tk.wm_title(root, "ELEC 391 GUI")
+    canvas = FigureCanvasTkAgg(f, root)
+    canvas.show()
+    canvas.get_tk_widget().place(x=0,y=100)
+    toolbar = NavigationToolbar2TkAgg(canvas, root)
+    toolbar.update()
+    canvas._tkcanvas.place(x=0,y=35)
+    canvas.callbacks.connect('button_press_event', on_click)
+    return root 
+ 
+def init_serial():
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.port = 'COM4'
+    ser.timeout = 1
+    ser.open()
+    return ser
+
+def draw_point(x,y):
+    global rect, pause, right_length, line1,line2,line3,line4, x_conv, y_conv, restrict
+    if pause == 0:
+        w.delete(rect)
+        w.delete(line1)
+        w.delete(line2)
+        w.delete(restrict)
+        rect = w.create_rectangle(x-1,wHeight-y-1,x+3,wHeight-y+3, outline="")
+        line1 = w.create_line(x_conv*(x_range/4), wHeight+(y_conv*y_start), x_conv*(x+x_range/2),(y_end-y)*y_conv, width=4 )
+        line2 = w.create_line(x_conv*(x_range*0.75), wHeight+(y_conv*y_start), x_conv*(x+x_range/2), (y_end-y)*y_conv, width=4 )
+        
+def pause_update():
+    ser.write(PauseByte)
+
+def clear():
+    global sqX, sqY, sqH, sqL
+    sqX = sqY = 0
+    sqH = sqL = 0
+    w.delete(ALL)
+   
+def close(event):
+    sys.exit()
+
+def on_click(event):
+    global mouse_x, mouse_y, manual_pos, sqX, sqY, sqL, sqH
+    mouse_x = event.x
+    mouse_y = event.y
+    manual_pos = 1
+    sqX = sqY = sqL = sqH = 0
+    Set_PID(10,0,1)
+    left_length, right_length = inverse_kinematic(mouse_x/x_conv - (x_range/2), y_end-mouse_y/y_conv)
+    ser.write(b"\xFB")
+    value = [int(left_length)]
+    angle = bytes(value)
+    ser.write(angle)
+    value = [int(right_length)]
+    angle = bytes(value)
+    ser.write(angle)
+    ser.write(b"\xFA")
+    Send_Intersect_Flag(TRUE)
+    
+
+def forward_kinematic(right_length, left_length):
+    global x_c, y_c, base_length
+    left_angle = CosLaw(right_length,left_length, base_length)
+    y_c = left_length*math.sin(left_angle)
+    x_c = left_length*math.cos(left_angle) - base_length/2;
+    return(x_c,y_c)
+
+def inverse_kinematic(x,y):
+    global base_length
+    left_angle = math.atan(y/x)
+    left_length = x/math.cos(left_angle)
+    right_angle = math.atan(y/(base_length-x))
+    right_length = y/math.sin(right_angle)
+    return left_length, right_length
+
+def CosLaw(desired_l,a,b):
+    angle = (a**2 + b**2 - desired_l**2)/(2*a*b)
+    return math.acos(angle)
+ 
+def MsgHandler(Msgtype, data):
+    global left_length, right_length, startrx
+    if Msgtype == eMsgType.encoderPosLeft:
+        if startrx == 1:
+            left_length = data
+
+            stleft_length.set("%.1lf", left_length)
+            stright_length.set("%.1lf", right_length)
+            
+    elif Msgtype == eMsgType.encoderPosRight:
+        if startrx == 1:       
+            right_length = data
+            x,y = forward_kinematic(right_length, left_length)
+            stXCoord.set("%.1lf", x)
+            stYCoord.set("%.1lf", y)
+            stright_length.set("%.1lf", right_length)
+            draw_point(x,y)
+    elif Msgtype == eMsgType.desiredLeft:
+        #stDesired1.set("%d", data)
+        pass
+    elif Msgtype == eMsgType.desiredRight:
+        #stDesired2.set("%d", data)
+        pass
+    elif Msgtype == eMsgType.debug:
+        print(data)
+    elif Msgtype == eMsgType.start:
+        startrx = 1;
+        
+def main():
+    global state, success_count, x_c, y_c, MsgLength, MsgType, data, manual_pos, mouse_x, mouse_y
+    #-----------------------------------------------------------------------------------------    
+    #-------------------------Transfer Protocol State Machine---------------------------------
+    #-----------------------------------------------------------------------------------------
+    if ser.inWaiting()>0:
+        if state == States.RxStart: #waiting to receive startbyte
+            x = int(ser.read(2),16)
+            if x == Startbyte: #received startbyte
+                state = States.RxMsgType
+        elif state == States.RxMsgType: #receive msg type, 1 byte
+            x = int(ser.read(1),16)
+            if x == 1:
+                MsgType = eMsgType.encoderPosLeft
+            elif x == 2:
+                MsgType = eMsgType.encoderPosRight
+            elif x == 3:
+                MsgType = eMsgType.desiredLeft
+            elif x == 4:
+                MsgType = eMsgType.desiredRight
+            elif x == 5:
+                MsgType = eMsgType.debug
+            elif x == 6:
+                MsgType = eMsgType.start
+            state = States.RxMsgLength
+        elif state == States.RxMsgLength: #receive message length, 1 byte
+            x = int(ser.read(1),16)
+            MsgLength = x
+            if(MsgLength): #if msg length is 0, rx fail and reset to wait for start
+                state = States.RxMsg
+            else:
+                state = States.RxStart
+        elif state == States.RxMsg: #receive msg length bytes of data 
+            data = int(ser.read(MsgLength),16)
+            state = States.RxEnd
+        elif state == States.RxEnd: #wait for endbyte, if received proceed to handle message, else go back to start          
+            x = int(ser.read(2),16)
+            if x == EndByte: #if endbyte is received then update x,y coordinate
+                success_count = success_count + 1
+                MsgHandler(MsgType, data)
+            #else discard bytes and change state back to waiting for startbyte
+            state = States.RxStart                         
+    #-----------------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------------------
+    
+    stRcvSuccess.set("%d", success_count)
+    
+    if not manual_pos:
+        if curr_shape == Shape.Rectangle:
+            #rectIntersect(x_c, y_c)
+            pass
+        elif curr_shape == Shape.Circle:
+            circIntersect(x_c, y_c)
+    if manual_pos == 1:
+        print(abs(x_c-(mouse_x/x_conv - (x_range/2))),abs(y_c-(y_end-mouse_y/y_conv)))
+        if(abs(x_c-(mouse_x/x_conv - (x_range/2))) <2 and abs(y_c-(y_end-mouse_y/y_conv))<2):
+            Send_Intersect_Flag(FALSE)
+    root.after(1,main)
+    
+#-----------------------------------------------------------------------------------------    
+#-------------------------------------Initialization--------------------------------------
+#-----------------------------------------------------------------------------------------    
+ser = init_serial()  
+root = init_window()
+create_buttons(root)
+create_labels(root)
+stRcvSuccess = StatusBar(root)
+stXCoord = StatusBar(root)
+stYCoord = StatusBar(root)
+stleft_length = StatusBar(root)
+stright_length = StatusBar(root)
+stDesired1 = StatusBar(root)
+stDesired2 = StatusBar(root)
+stRcvSuccess.place(x=780,y=10)
+stXCoord.place(x=780,y=40)
+stYCoord.place(x=780,y=70)
+stleft_length.place(x=670,y=120)
+stright_length.place(x=770,y=120)
+stDesired1.place(x=670, y=180)
+stDesired2.place(x=770, y=180)
+w = Canvas(root, width=wWidth, height=wHeight, highlightthickness = 0)
+w.place(x=82,y=98)
+rect = w.create_rectangle(0,0,0,0, fill="blue")
+line1 = w.create_line(0,0,0,0)
+line2 = w.create_line(0,0,0,0)
+line3 = w.create_line(0,0,0,0)
+line4 = w.create_line(0,0,0,0)
+restrict = w.create_rectangle(0,0,0,0)
+rectangle = w.create_rectangle(0,0,0,0)
+#-----------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+entryX = Entry(root, width = 5)
+entryX.place(x=760,y=300)
+entryY = Entry(root, width = 5)
+entryY.place(x=800,y=300)
+entryLength = Entry(root, width = 5)
+entryLength.place(x=760, y=325)
+entryHeight = Entry(root, width = 5)
+entryHeight.place(x=760, y=350)
+entryP = Entry(root,width =5)
+entryP.place(x=710, y= 450)
+entryI = Entry(root,width =5)
+entryI.place(x=760, y= 450)
+entryD = Entry(root,width =5)
+entryD.place(x=810, y= 450)
+XYLabel = Label(root)
+XYLabel.place(x=660,y=300)
+LengthLabel = Label(root)
+LengthLabel.place(x=660,y=325)
+WidthLabel = Label(root)
+WidthLabel.place(x=660,y=350)
+PIDLabel = Label(root)
+PIDLabel.place(x=720,y=470)
+PIDLabel.config(text="P    I    D", font = "Courier 12 bold")
+Label(root, text="Press Enter To Set", font = "Courier 12 bold").place(x=660, y=380)
+Label(root, text="Constraint Region", font = "Courier 12 bold").place(x=660, y=270)
+#-----------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+draw_constraint(Shape.Rectangle, default_sqX, default_sqY, default_sqL, default_sqH)
+root.bind('<Escape>', close)
+root.bind('<Return>', entry_callback)
+w.bind('<Button-1>', on_click)
+root.after(1,main)
+root.mainloop()
+
+#more shapes
+#images
+#custom shapes   
+#multiple shapes at once
+#bind keys to do something
+#choose spring, wall, button, etc 
+#keep in/keep out
